de1e25aad7210d52705f5b123802c7dcbe77b447
Robin Genz <mail@robingenz.dev>
2023-04-02 21:18:52 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.robingenz.capacitorjs.demo.firebasemessaging',
+  appName: 'capacitor-firebase-messaging-demo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
