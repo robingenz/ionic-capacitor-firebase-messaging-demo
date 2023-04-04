c3f8199d65247000dee88815a4475d462cfa3f4b
Robin Genz <mail@robingenz.dev>
2023-04-04 22:53:25 +0200

    Add VAPID Key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,7 @@ export const environment = {
     projectId: "capacitor-messaging-demo",
     storageBucket: "capacitor-messaging-demo.appspot.com",
     messagingSenderId: "153571302575",
-    appId: "1:153571302575:web:3dec9634598de1bff0885a"
+    appId: "1:153571302575:web:3dec9634598de1bff0885a",
+    vapidKey: "BKqmc3jyJHytC_sab0f4mkAnkZcQMArZaUfZAWWkSpp0SK4wgnfA_EYY3KKAOZIocIu92zQnqCTiA0_Lf_5bAyg"
   },
 };
