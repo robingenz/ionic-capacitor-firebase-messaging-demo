0b2d9bbdf060423b68dc81ff1a5870a3d3cab014
Robin Genz <mail@robingenz.dev>
2023-04-04 23:06:30 +0200

    Update the `HomePage` component

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { FirebaseMessaging } from '@capacitor-firebase/messaging';
+import { Capacitor } from '@capacitor/core';
 import { IonicModule } from '@ionic/angular';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +12,31 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule],
 })
 export class HomePage {
-  constructor() {}
+  public token = '';
+
+  constructor() {
+    FirebaseMessaging.addListener('notificationReceived', (event) => {
+      console.log('notificationReceived: ', { event });
+    });
+    FirebaseMessaging.addListener('notificationActionPerformed', (event) => {
+      console.log('notificationActionPerformed: ', { event });
+    });
+
+    if (!Capacitor.isNativePlatform()) {
+      navigator.serviceWorker.addEventListener('message', (event: any) => {
+        console.log('serviceWorker message: ', { event });
+      });
+    }
+  }
+
+  public async requestPermissions(): Promise<void> {
+    await FirebaseMessaging.requestPermissions();
+  }
+
+  public async getToken(): Promise<void> {
+    const { token } = await FirebaseMessaging.getToken({
+      vapidKey: environment.firebase.vapidKey,
+    });
+    this.token = token;
+  }
 }
